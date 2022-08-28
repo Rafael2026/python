ab5ccc738f27a0ba6916ebf42b35bab902304851
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-08-28 13:25:52 +0200

    Practice python
    Signed-off-by: Rafael2026 <rafaaguilarmunoz@gmail.com>

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 ## Official website
 <a href="https://www.python.org/downloads/">Download Python</a>
+
+## Check your python version
+<p>python --version</p>
+
+## Tutorials
+<a href="https://www.w3schools.com/python/default.asp">Official python tutorial - W3Schools</a>
---
+++ w3Schools.py
@@ -0,0 +1,22 @@
+print("Hello, World!") # This is a comment.
+
+if 5 > 2:
+  print("Five is greater than two!")
+  
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
