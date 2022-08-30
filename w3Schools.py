6e0ba13a4f053d4e59f06b596886e65301e26142
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-08-30 15:28:40 +0200

    More python content
    Signed-off-by: Rafael2026 <rafaaguilarmunoz@gmail.com>

--- w3Schools.py
+++ w3Schools.py
@@ -1,13 +1,15 @@
 print("Hello, World!") # This is a comment.
 
-if 5 > 2:
-  print("Five is greater than two!")
-  
-x = 5
-y = "Hello, World!"
-
-print(x)
-print(y)
+numero1 = 5
+saludo = "Hello, World!"
+numero2 = 4
+nombre = "Sally"
+lista = ["apple", "banana", "cherry"]
+dictionary = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
 
 """
 This is a comment
@@ -15,8 +17,26 @@ written in
 more than just one line
 """
 
-a = 4
-A = "Sally"
+if 5 > 2:
+  print("Five is greater than two!")
 
-print(a)
-print(A)
+print(numero1)
+print(saludo)
+print(numero2)
+print(nombre)
+print(type(1))
+print(type(2.8))
+print(type(1j))
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print(lista)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(thisdict["brand"])
+print(type(dictionary))
